8be29787189ad6a92232ee33150fbe1ff487f6c4
Elliot Park <elliotpark410@gmail.com>
2022-01-31 00:55:21 -0800

    add more comments

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,44 +1,53 @@
-// Create function to dynamically generateHTML 
+// Create function to dynamically generateHTML using employeeData (inquirer prompt user input)
 function generateHTML(employeeData) {
+
+  // Create an array to determine which cards need to be made
   employeeCardsArray = [];
 
+  // Create a for loop to capture all the ojbects in the array and get the employee object and role
   for (let i = 0; i < employeeData.length; i++) {
     const employee = employeeData[i];
     const role = employee.getRole()
 
-
+    // if role equals "Manager", create a manager card
     if (role === 'Manager') {
-      const managerCard = generateManager(employee);
+      const managerCard = generateManagerCard(employee);
 
+      // push managerCard to employeeCardsArray to eventually generate team cards
       employeeCardsArray.push(managerCard);      
     }
 
+    // if role equals "Engineer", create an engineer card
     if (role === 'Engineer') {
-      const engineerCard = generateEngineer(employee);
+      const engineerCard = generateEngineerCard(employee);
 
+      //  push engineerCard to employeeCardsArray to eventually generate team cards
       employeeCardsArray.push(engineerCard);      
     }
 
+    // if role equals "Intern", create an intern card
     if (role === 'Intern') {
-      const internCard = generateIntern(employee);
+      const internCard = generateInternCard(employee);
 
+      //  push internCard to employeeCardsArray to eventually generate team cards
       employeeCardsArray.push(internCard);      
     }
   }
 
-
+  // .join('') returns the employeeCardsArray as a string which is needed for creating the html page
   const employeeCards = employeeCardsArray.join('');
-  console.log(employeeCards);
-
-  const generateTeam = generateTeamPage(employeeCards);
+  
+  // call function generateTeamCards with employeeCards argument 
+  // return generateTeam to create html
+  const generateTeam = generateTeamCards(employeeCards);
   return generateTeam;
 }
 
 
 
 
-
-function generateManager (manager) {
+// generateManagerCard with manager object and template literals
+function generateManagerCard (manager) {
   return `
     <div class="card mx-4 mb-4" id="manager-card">
       <div class="card-header text-center card-title bg-info text-white">
@@ -58,7 +67,8 @@ function generateManager (manager) {
 }
 
 
-function generateEngineer (engineer) {
+// generateEngineerCard with engineer object and template literals
+function generateEngineerCard (engineer) {
   return `
     <div class="card mx-4 mb-4" id="engineer-card">
       <div class="card-header text-center card-title bg-info text-white">
@@ -78,7 +88,8 @@ function generateEngineer (engineer) {
 }
 
 
-function generateIntern (intern) {
+// generateInternCard with intern object and template literals
+function generateInternCard (intern) {
   return `
     <div class="card mx-4 mb-4" id="intern-card">
       <div class="card-header text-center card-title bg-info text-white">
@@ -98,7 +109,8 @@ function generateIntern (intern) {
 }
 
 
-function generateTeamPage(employeeCards) {
+// generateTeamCard with employeeCards argument and template literals
+function generateTeamCards(employeeCards) {
   return `
   <!DOCTYPE html>
   <html lang="en">
@@ -164,5 +176,5 @@ function generateTeamPage(employeeCards) {
   `;
 }
 
-
+// Export generateHTML module so Node.js tells oher files are allowed to access the code
 module.exports = generateHTML;  
